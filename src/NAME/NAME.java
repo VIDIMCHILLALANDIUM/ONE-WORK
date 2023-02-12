e078ae0a76778500f2c1432bc69656f352ced042
VADIMCHILLALANDIUM <pravilov05022001@gmail.com>
2023-02-12 19:33:05 +1000

    ONE WORK

---
+++ src/NAME/NAME.java
@@ -0,0 +1,14 @@
+package NAME;
+
+public class NAME {
+    public static void main(String[] args) {
+        System.out.println("*       *      *      ****    *   *     *");
+        System.out.println(" *     *      * *     *   *      * *   * *");
+        System.out.println("  *   *      *   *    *    *  *  *  * *  *");
+        System.out.println("   * *      *******   *   *   * *    *    *");
+        System.out.println("    *      *       *  ****    * *         *");
+        System.out.println("");
+        System.out.printf("1000-7=");
+        System.out.println(1000 - 7);
+    }
+}
